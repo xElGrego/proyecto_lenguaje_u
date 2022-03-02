77c37e4038a320688830e79d0d8ee41a43538e35
xElgrego <gregorio745@hotmail.com>
2022-03-02 00:44:50 -0500

    login page y register page

---
+++ lib/src/app/pages/login/login_page.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+
+import '../../widgets/background.dart';
+import '../register/register_page.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return SafeArea(
+      child: Scaffold(
+        body: Background(
+          child: ListView(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top:18.0),
+                child: Container(
+                  alignment: Alignment.centerLeft,
+                  padding: const EdgeInsets.symmetric(horizontal: 40),
+                  child: const Text(
+                    "Bienvenido",
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, color: Color(0xFF2661FA), fontSize: 36),
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+              ),
+              SizedBox(height: size.height * 0.03),
+              Container(
+                alignment: Alignment.center,
+                margin: const EdgeInsets.symmetric(horizontal: 40),
+                child: const TextField(
+                  decoration: InputDecoration(labelText: "Usuario"),
+                ),
+              ),
+              SizedBox(height: size.height * 0.03),
+              Container(
+                alignment: Alignment.center,
+                margin: const EdgeInsets.symmetric(horizontal: 40),
+                child: const TextField(
+                  decoration: InputDecoration(labelText: "Contraseña"),
+                  obscureText: true,
+                ),
+              ),
+              Container(
+                alignment: Alignment.centerRight,
+                margin: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+                child: const Text(
+                  "¿Olvidaste la contraseña?",
+                  style: TextStyle(fontSize: 12, color: Color(0XFF2661FA)),
+                ),
+              ),
+              SizedBox(height: size.height * 0.05),
+              Container(
+                alignment: Alignment.centerRight,
+                margin: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+                child: RaisedButton(
+                  onPressed: () {},
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(80.0),
+                  ),
+                  textColor: Colors.white,
+                  padding: const EdgeInsets.all(0),
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 50.0,
+                    width: size.width * 0.5,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(80.0),
+                        gradient: const LinearGradient(colors: [
+                          Color.fromARGB(255, 255, 136, 34),
+                          Color.fromARGB(255, 255, 177, 41)
+                        ])),
+                    padding: const EdgeInsets.all(0),
+                    child: const Text(
+                      "LOGIN",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.centerRight,
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 40,
+                  vertical: 10,
+                ),
+                child: GestureDetector(
+                  onTap: () => {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const RegisterPage(),
+                      ),
+                    ),
+                  },
+                  child: const Text(
+                    "¿No tienes una cuenta? Créala",
+                    style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF2661FA),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/app/widgets/background.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class Background extends StatelessWidget {
+  final Widget child;
+
+  const Background({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final orientation = MediaQuery.of(context).orientation;
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    return Container(
+      child: orientation == Orientation.portrait
+          ? SizedBox(
+              width: width,
+              height: height,
+              child: Stack(
+                alignment: Alignment.center,
+                children: <Widget>[
+                  Positioned(
+                    top: 0,
+                    right: 0,
+                    child: Image.asset("assets/images/top1.png", width: width),
+                  ),
+                  Positioned(
+                    top: 0,
+                    right: 0,
+                    child: Image.asset("assets/images/top2.png", width: width),
+                  ),
+
+                  Positioned(
+                    bottom: 0,
+                    right: 0,
+                    child: Image.asset("assets/images/bottom1.png", width: width),
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    right: 0,
+                    child: Image.asset("assets/images/bottom2.png", width: width),
+                  ),
+                  child
+                ],
+              ),
+            )
+          : SizedBox(
+              width: width,
+              height: height,
+              child: Stack(
+                alignment: Alignment.center,
+                children: <Widget>[
+                
+                  child
+                ],
+              ),
+            )
+    );
+  }
+}
